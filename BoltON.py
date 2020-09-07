a7c713a2cbdea49230eaf5ae8f8f9ac44d712997
Simona B <43524226+BellatrixLeStranger@users.noreply.github.com>
2020-09-08 00:57:22 +0100

    Fix various issues and add more functionality

--- BoltON.py
+++ BoltON.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands # commands extension
 
-# create instance of a bot(client) and set command prefix
+# create instance of a bot(client) and set desired command prefix
 client = commands.Bot(command_prefix = ".")
 
 # event that outputs when the bot is online
@@ -9,28 +9,64 @@ client = commands.Bot(command_prefix = ".")
 async def on_ready():
     print("BoltON bot is ON.")
 
-# output when a member object joins the server
+# output in a specific channel when a member object joins the server
 @client.event
 async def on_member_join(member):
-   print(f'{member} has joined the server. :yas_homer:')
+   await client.get_channel(channel ID here).send(f'Welcome {member.mention}! \n'
+   'Please head over to the <#channel ID here> channel to choose your role.')
 
-# output when a member object leaves the server
+# output in a specific channel when a member object leaves the server 
 @client.event
 async def on_member_remove(member):
-   print(f'{member} has left the server. :wee:')
+   mention_user = member.mention
+   await client.get_channel(channel ID here).send(f"{mention_user} has left the server. ")
 
 # command to check the latency of the bot in miliseconds; ctx = context
 @client.command()
 async def ping(ctx):
-   await ctx.send(f'I"m on! :hehe: {round(client.latency * 1000)}')
+   await ctx.send(f"I'm on! My latency is {round(client.latency * 1000)} ms.")
 
 # command to clear the messages from a channel(chat room)
 @client.command()
-async def purger(ctx, amount=5):
+# restrict the command to a specific role  
+@commands.has_role("role name here")
+async def purger(ctx, amount = 5):
    if amount > 0:
-      await ctx.channel.purge(limit=amount)
+      await ctx.channel.purge(limit = amount)
    else:
-      await ctx.send("Am I joke to you? :really: ")
+      await ctx.send("Am I joke to you?")
+# outputs an error message when the command is run by a different role
+@purger.error
+async def purger_error(ctx, error):
+   if isinstance(error, commands.MissingRole):
+      await ctx.send("You must be <role name here> to run this command.")
+
+# command to kick an user
+@client.command()
+# restrict the command to specific role
+@commands.has_role("role name here")
+async def kick(ctx, member : discord.Member, *, why = None):
+   await member.kick(reason = why)
+# outputs an error message when the command is run by a different role
+@kick.error
+async def kick_error(ctx, error):
+   if isinstance(error, commands.MissingRole):
+      await ctx.send("You must be <role name here> to run this command.")
+
+# command to ban an user
+@client.command()
+# restrict the command to specific role
+@commands.has_role("role name here")
+async def ban(ctx, member : discord.Member, *, why = None):
+   await member.ban(reason = why)
+# outputs an error message when the command is run by a different role
+@ban.error
+async def ban_error(ctx, error):
+   if isinstance(error, commands.MissingRole):
+      await ctx.send("You must be <role name here> to run this command.")
+
+
+
 
 # run the bot
 client.run('your own bot token here')
--- README.md
+++ README.md
@@ -6,5 +6,6 @@ A management bot for discord servers.
 
 - output message in a channel when an user joins/leaves server 
 - show latency in miliseconds
-- clear messages in a channel(chat room)
+- clear messages in a channel(chat room) - optional role-restriction
+- kick/ban users - optional role-restriction(recommended)
 - role assignment (to be continued) 
